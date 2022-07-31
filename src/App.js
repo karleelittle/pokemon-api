ac076ac4c24cd90fde3c6d32ed91b1ceba897580
Karlee Little <karleelittle1@gmail.com>
2022-07-31 00:16:58 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+          .then(response => {
+            return response.json()
+        })
+        .then(response => {
+            setPokemon(response.results)
+        })
+        .catch((err) =>{
+            console.log(err);
+        })
+
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        { pokemon.map((pokemonObj, index) =>{
+          return (<li key={index}>{pokemonObj.name} </li>)
+        })
+        }
+      </ul>
     </div>
   );
 }
